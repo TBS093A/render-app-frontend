098270c434d81cccb503462a89d7c2eb74de8e61
TBS093A <zukkamil.44@gmail.com>
2021-02-17 15:56:48 +0100

    Redux / React -> integrate views & pages

---
+++ src/pages/func_group/render_websocket/renderWebsocketIndex.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+const RenderWebsocketIndex = () => {
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default RenderWebsocketIndex
