a75dcf97a9de10888ad1aa0f308ebac92cff9166
TBS093A <zukkamil.44@gmail.com>
2021-02-17 16:29:15 +0100

    Redux / React -> add movements

--- src/pages/func_group/root.js
+++ src/pages/func_group/root.js
@@ -1,21 +1,19 @@
 import React, { useState, useEffect } from 'react'
 
+import VerifyUserSession from './rootUtils'
+
 import UserAuthIndex from './user_auth/userAuthIndex'
 import UserCrudIndex from './user_crud/userCrudIndex'
 import ModelCrudIndex from './model_crud/modelCrudIndex'
 import RenderCrudIndex from './render_crud/renderCrudIndex'
 import RenderWebsocketIndex from './render_websocket/renderWebsocketIndex'
 
-const Root = () => {
 
-    const [userAuthView, setUserAuthView] = useState(true)
-    const [userCrudView, setUserCrudView] = useState(false)
-    const [modelCrudView, setModelCrudView] = useState(false)
-    const [renderCrudView, setRenderCrudView] = useState(false)
-    const [renderWebsocketView, setRenderWebsocketView] = useState(false)
+const Root = () => {
 
     return (
         <div>
+            <VerifyUserSession />
         </div>
     )
 }
