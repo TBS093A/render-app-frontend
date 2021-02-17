675e8b3452453d9c23f9020c054d57b8a4a781b2
TBS093A <zukkamil.44@gmail.com>
2021-02-17 15:56:48 +0100

    Redux / React -> integrate views & pages

---
+++ src/pages/func_group/model_crud/modelCrudIndex.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+const ModelCrudIndex = () => {
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default ModelCrudIndex
---
+++ src/pages/func_group/user_crud/userCrudIndex.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+const UserCrudIndex = () => {
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default UserCrudIndex
